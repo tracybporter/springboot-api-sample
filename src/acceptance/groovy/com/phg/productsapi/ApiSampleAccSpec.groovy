bb6cb022edccc5fe89b57356baf5df5bcf0f859d
Tracy B. Porter <tbporter@gmail.com>
2016-10-07 09:24:07 -0500

    Add version file (#7)
    * tracy - report test counts at command line
    
    * tracy - using stubby4j as dependency rather than embedded jar file.
    
    * tracy - enabled /actuator endpoint.
    
    * tracy - acceptance for app version failing as expected.
    
    * tracy - /info response include application version.
    
    * tracy - cleaned up annotations.

--- src/acceptance/groovy/com/phg/productsapi/ApiSampleAccSpec.groovy
+++ src/acceptance/groovy/com/phg/productsapi/ApiSampleAccSpec.groovy
@@ -5,22 +5,25 @@ import org.springframework.boot.test.IntegrationTest
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
+import spock.lang.Ignore
 import spock.lang.Specification
 
 @ContextConfiguration(classes = MainApplication.class, loader = SpringApplicationContextLoader.class)
 @WebAppConfiguration
 @IntegrationTest
-class RootAccSpec extends Specification {
-   def "default root response from lazybones sample"() {
-      given:
-      RESTClient restClient = new RESTClient('http://localhost:8080')
+class ApiSampleAccSpec extends Specification {
 
-      when:
-      def response = restClient.get(path: '/')
+    def "/info returns application version"() {
+        given:
+        RESTClient restClient = new RESTClient('http://localhost:8080')
 
-      then:
-      response.status == 200
-      response.headers.'Content-Type'.toString() == 'application/json;charset=UTF-8'
-      response.data.message == 'Hello World'
-   }
+        when:
+        def response = restClient.get(path: '/info')
+        def data = response.data
+
+        then:
+        response.status == 200
+        response.headers.'Content-Type'.toString() == 'application/json;charset=UTF-8'
+        data.app.version == '0.1.1'
+    }
 }
--- src/main/groovy/com/phg/productsapi/MainApplication.groovy
+++ src/main/groovy/com/phg/productsapi/MainApplication.groovy
@@ -1,26 +1,16 @@
-
 package com.phg.productsapi
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.annotation.PropertySource
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@RestController
+@SpringBootApplication
+@PropertySource('classpath:/appversion.properties')
 class MainApplication {
 
-   @RequestMapping("/")
-   def helloWorld() {
-      [message: "Hello World"]
-   }
-
-   static void main(String[] args) throws Exception {
-      SpringApplication.run(MainApplication.class, args)
-   }
+    static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args)
+    }
 
 }
